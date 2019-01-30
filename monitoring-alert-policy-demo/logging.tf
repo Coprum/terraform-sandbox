ef6ee316849432a0cffa68b654234804f9a9bd6d
高木亮太郎 <takagiryoutarou@TKG-MBP.local>
2019-01-30 20:30:44 +0900

    add depends_on

--- monitoring-alert-policy-demo/logging.tf
+++ monitoring-alert-policy-demo/logging.tf
@@ -1,5 +1,7 @@
 # Create Log Sink 
 resource "google_logging_project_sink" "instance-log" {
+    depends_on = ["google_storage_bucket.bucket"]
+    
     name = "compute-instance-sink"
 
     destination = "storage.googleapis.com/${google_storage_bucket.bucket.name}"
